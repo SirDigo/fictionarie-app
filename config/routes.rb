b3cbc2101b9d0f4ad6311a2ada396b2680ca38a8
Caleb Rodriguez <sirdigo6@gmail.com>
2022-02-21 14:42:31 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  #testing sessions
+  # get '/hello', to: 'application#hello_world'
+end
